6da3478fa6f48474d682978384c0cf2515d55872
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2026-07-30 21:24:01 +0300

    feat(ui): add horizontal mobile nav under header with theme toggle

--- src/components/layout/SiteLayout.tsx
+++ src/components/layout/SiteLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import ScrollToTopButton from "../ScrollToTopButton";
 import { Header } from "./Header";
+import { MobileNav } from "./MobileNav";
 
 export function SiteLayout() {
   return (
     <div className="site-layout">
       <Header />
+      <MobileNav />
       <div id="main-content" className="site-layout-content">
         <Outlet />
       </div>
